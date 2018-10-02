3561b3112e3ccb7c6fa3eace5c09e00b92b867a9
Facundo <facaagus@gmail.com>
2018-10-02 01:21:02 -0300

    Falta mucho y arreglar

---
+++ TP_2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Biblioteca.h"
+
+int main()
+{
+    sEmployee listadoEmployee[TAM];
+    inicializarArrayIlogicamente(listadoEmployee);
+    menu(listadoEmployee);
+}
